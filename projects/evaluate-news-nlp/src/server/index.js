d5f223beca7fc31dcc39ba0ae13b34f24865a9b6
Momotaro-Yvv <84366910+Momotaro-Yvv@users.noreply.github.com>
2022-01-15 23:08:11 +1100

    Commit before api

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,21 +1,33 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+dotenv.config();
 
-const app = express()
+const aylien = require('"aylien_textapi"');
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+
+const app = express();
+
+// Cors for cross origin allowance(beacuse by default only origin is allowed)
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.static('dist'))
+
+var textapi = new aylien({
+    application_key: process.env.API_KEY
+ });
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
